dce5474d3f2905951a39abf080f365d28866cec1
ArFFy <2470958990@qq.com>
2022-08-24 09:07:13 +0800

    继承的基本语法

---
+++ 127_inherit_basic.cpp
@@ -0,0 +1,182 @@
+//继承 基本语法
+//减少重复代码
+//class 子类 ：继承方式 父类
+//子类又称派生类 父类又称基类
+#include<iostream>
+using namespace std;
+//非继承写法
+//Java页面
+class Java
+{
+public:
+	void header()
+	{
+		cout << "首页、公开课、登录、注册...（公共头部）" << endl;
+	}
+	void footer()
+	{
+		cout << "帮助中心、交流合作、站内地图...(公共底部)" << endl;
+	}
+	void left()
+	{
+		cout << "Java,Python,C++...(公共分类列表)" << endl;
+	}
+	void content()
+	{
+		cout << "JAVA学科视频" << endl;
+	}
+};
+//Python页面
+class Python
+{
+public:
+	void header()
+	{
+		cout << "首页、公开课、登录、注册...（公共头部）" << endl;
+	}
+	void footer()
+	{
+		cout << "帮助中心、交流合作、站内地图...(公共底部)" << endl;
+	}
+	void left()
+	{
+		cout << "Java,Python,C++...(公共分类列表)" << endl;
+	}
+	void content()
+	{
+		cout << "Python学科视频" << endl;
+	}
+};
+//C++页面
+class CPP
+{
+public:
+	void header()
+	{
+		cout << "首页、公开课、登录、注册...（公共头部）" << endl;
+	}
+	void footer()
+	{
+		cout << "帮助中心、交流合作、站内地图...(公共底部)" << endl;
+	}
+	void left()
+	{
+		cout << "Java,Python,C++...(公共分类列表)" << endl;
+	}
+	void content()
+	{
+		cout << "C++学科视频" << endl;
+	}
+};
+
+void test01()
+{
+	//Java页面
+	cout << "Java下载视频页面如下： " << endl;
+	Java ja;
+	ja.header();
+	ja.footer();
+	ja.left();
+	ja.content();
+	cout << "--------------------" << endl;
+
+	//Python页面
+	cout << "Python下载视频页面如下： " << endl;
+	Python py;
+	py.header();
+	py.footer();
+	py.left();
+	py.content();
+	cout << "--------------------" << endl;
+
+	//C++页面
+	cout << "C++下载视频页面如下： " << endl;
+	CPP cp;
+	cp.header();
+	cp.footer();
+	cp.left();
+	cp.content();
+
+}
+//继承写法
+//公共页面类
+class BasePage
+{
+public:
+	void header()
+	{
+		cout << "首页、公开课、登录、注册...（公共头部）" << endl;
+	}
+	void footer()
+	{
+		cout << "帮助中心、交流合作、站内地图...(公共底部)" << endl;
+	}
+	void left()
+	{
+		cout << "Java,Python,C++...(公共分类列表)" << endl;
+	}
+};
+//java页面
+class Java1 :public BasePage
+{
+public:
+	void content()
+	{
+		cout << "java学科视频" << endl;
+	}
+};
+//python页面
+class Python1 :public BasePage
+{
+public:
+	void content()
+	{
+		cout << "Python学科视频" << endl;
+	}
+};
+//c++页面
+class CPP1 :public BasePage
+{
+public:
+	void content()
+	{
+		cout << "Cpp学科视频" << endl;
+	}
+};
+void test02()
+{
+	//Java页面
+	cout << "Java下载视频页面如下： " << endl;
+	Java1 ja;
+	ja.header();
+	ja.footer();
+	ja.left();
+	ja.content();
+	cout << "--------------------" << endl;
+
+	//Python页面
+	cout << "Python下载视频页面如下： " << endl;
+	Python1 py;
+	py.header();
+	py.footer();
+	py.left();
+	py.content();
+	cout << "--------------------" << endl;
+
+	//C++页面
+	cout << "C++下载视频页面如下： " << endl;
+	CPP1 cp;
+	cp.header();
+	cp.footer();
+	cp.left();
+	cp.content();
+
+}
+int main() {
+
+	test01();
+	test02();
+	system("pause");
+
+	return 0;
+}
