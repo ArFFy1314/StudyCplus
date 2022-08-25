860cc9e7a3270886348af45e8ec554101eebe347
ArFFy <2470958990@qq.com>
2022-08-25 11:39:09 +0800

    文件 二进制 写

---
+++ 145_filebinary_write.cpp
@@ -0,0 +1,25 @@
+//文件 二进制 写
+#include<iostream>
+using namespace std;
+#include<fstream>
+
+class Person
+{
+public:
+	char m_Name[64];
+	int m_Age;
+};
+void test01()
+{
+	ofstream ofs;
+	ofs.open("person.txt", ios::out | ios::binary);
+	Person p = { "张三",18 };
+	ofs.write((const char*)&p, sizeof(Person));
+	ofs.close();
+}
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
