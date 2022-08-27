1f62fc7da636c39dc08f054bf6796d16f618aa54
ArFFy <2470958990@qq.com>
2022-08-27 19:03:54 +0800

    全局区

---
+++ 085_memory_globle.cpp
@@ -0,0 +1,60 @@
+﻿//**全局区：**
+//
+//​		全局变量和静态变量存放在此.
+//
+//​		全局区还包含了常量区, 字符串常量和其他常量也存放在此.
+//
+//​ == 该区域的数据在程序结束后由操作系统释放 == .
+//
+//总结：
+//
+//* C++中在程序运行前分为全局区和代码区
+//* 代码区特点是共享和只读
+//* 全局区中存放全局变量、静态变量、常量
+//* 常量区中存放 const修饰的全局常量  和 字符串常量
+#include<iostream>
+using namespace std;
+//全局变量
+int g_a = 10;
+int g_b = 10;
+
+//全局常量
+const int c_g_a = 10;
+const int c_g_b = 10;
+
+int main() {
+
+	//局部变量
+	int a = 10;
+	int b = 10;
+
+	//打印地址
+	cout << "局部变量a地址为： " << (int)&a << endl;
+	cout << "局部变量b地址为： " << (int)&b << endl;
+
+	cout << "全局变量g_a地址为： " << (int)&g_a << endl;
+	cout << "全局变量g_b地址为： " << (int)&g_b << endl;
+
+	//静态变量
+	static int s_a = 10;
+	static int s_b = 10;
+
+	cout << "静态变量s_a地址为： " << (int)&s_a << endl;
+	cout << "静态变量s_b地址为： " << (int)&s_b << endl;
+
+	cout << "字符串常量地址为： " << (int)&"hello world" << endl;
+	cout << "字符串常量地址为： " << (int)&"hello world1" << endl;
+
+	cout << "全局常量c_g_a地址为： " << (int)&c_g_a << endl;
+	cout << "全局常量c_g_b地址为： " << (int)&c_g_b << endl;
+
+	const int c_l_a = 10;
+	const int c_l_b = 10;
+	cout << "局部常量c_l_a地址为： " << (int)&c_l_a << endl;
+	cout << "局部常量c_l_b地址为： " << (int)&c_l_b << endl;
+
+	system("pause");
+
+	return 0;
+}
+
