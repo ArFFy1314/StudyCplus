7c9079a0b9c5f9e336489b40334bb004ab53f006
ArFFy <2470958990@qq.com>
2022-08-27 19:04:58 +0800

    引用 基本概念

---
+++ 089_cite_basic.cpp
@@ -0,0 +1,22 @@
+//引用的基本概念
+//**作用： **给变量起别名
+//**语法：** `数据类型 &别名 = 原名`
+#include<iostream>
+using namespace std;
+int main() {
+
+	int a = 10;
+	int &b = a;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	b = 100;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	system("pause");
+
+	return 0;
+}
